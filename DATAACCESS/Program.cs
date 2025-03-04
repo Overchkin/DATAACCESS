8ae6e3de3f16f50cea699597ec43e6a1c26cd16f
Israël TODOME <tdonanon@outlook.fr>
2025-03-04 16:03:36 -0500

    Mise à jour

--- DATAACCESS/Program.cs
+++ DATAACCESS/Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Microsoft.Data.SqlClient;
 
 namespace DATAACCESS
 {
@@ -10,6 +11,30 @@ namespace DATAACCESS
     {
         static void Main(string[] args)
         {
+            SqlConnection connection = new SqlConnection
+                ("Server=localhost,1433;Database=ApprendreCsharp;User Id=sa;Password=Passw0rd*; " +
+                "TrustServerCertificate=True");
+
+            connection.Open();
+            try
+            {
+                Console.WriteLine("Connexion ouverte !");
+                SqlCommand command = connection.CreateCommand();
+                command.CommandText = "INSERT INTO Personnes(Nom, Prenom) VALUES('SNOW', 'John')";
+                var result = command.ExecuteNonQuery();
+                if (result > 0)
+                {
+                    System.Console.WriteLine("Insertion effectuée avec succès");
+                }
+            }
+            catch (System.Exception)
+            {
+                throw;
+            }
+            finally
+            {
+                connection.Close();
+            }
         }
     }
 }
